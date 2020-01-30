7cf1898d9eecf48629f65c0a0be392ef47d23518
Zhejian Zhao <cupidzhaozhejian@gmail.com>
2020-01-30 15:00:38 -0500

    Updates

---
+++ precache-manifest.0adee7a046d0c5af7928a3da1ce2a239.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b76e62c718caba1c1ab1a03bcd1994f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52cf9968f0dd82c806a7",
+    "url": "/static/js/2.29a618ec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.29a618ec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6e03a9ad681374c72bc5",
+    "url": "/static/js/main.b1294478.chunk.js"
+  },
+  {
+    "revision": "0dda34acc7b81aad8062",
+    "url": "/static/js/runtime-main.c399816e.js"
+  }
+]);
